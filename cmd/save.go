aacce30e3a2f78d8e07e21fdaea11a87240a9889
Max <maxk893@gmail.com>
2021-02-26 12:02:47 -0800

    Added validation of mac address before attempting to save it to database.

--- cmd/save.go
+++ cmd/save.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"net"
 
 	"database/sql"
 
@@ -16,16 +18,22 @@ var saveCmd = &cobra.Command{
 	Long: `Saves a computer's MAC address, giving it a unique ID, along with an alias for that address.
 Wakie can use either alias or ID to lookup saved MAC Address`,
 	Run: func(cmd *cobra.Command, args []string) {
-		db, err := sql.Open("sqlite3", "/home/max/.config/wakie/wakie.db")
+		// First validate that MAC address is in the correct format
+		formattedMAC, err := net.ParseMAC(args[0])
 		if err != nil {
-			fmt.Printf("Error opening database file. %s", err)
+			log.Fatalf("Error validating MAC address. Please double check the address entered. \n %s", err)
+		}
+
+		db, err := sql.Open("sqlite3", dbPath)
+		if err != nil {
+			log.Fatalf("Error opening database file. %s", err)
 		}
 
 		flagValue, err := cmd.Flags().GetString("alias")
 
 		sqlStatement, err := db.Prepare("INSERT INTO 'main'.'computers'('MAC_Address', 'Alias') VALUES(?, ?);")
 
-		insertEntry, err := sqlStatement.Exec(args[0], flagValue)
+		insertEntry, err := sqlStatement.Exec(formattedMAC, flagValue)
 		if err != nil {
 			fmt.Printf("Error inserting data: %s", err)
 		}
