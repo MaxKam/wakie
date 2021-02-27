cab470922725753d8f5f0622469bff680057ba8f
Max <maxk893@gmail.com>
2021-02-26 16:11:59 -0800

    Created list command.

--- cmd/list.go
+++ cmd/list.go
@@ -1,51 +1,56 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"os"
 
+	"github.com/jedib0t/go-pretty/v6/table"
+	_ "github.com/mattn/go-sqlite3" // Driver for sql
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List all saved computers",
+	Long:  "List all saved computers in database. Will print out ID, Mac Address, and Alias.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		db, err := sql.Open("sqlite3", dbPath)
+		if err != nil {
+			log.Fatalf("Error opening database file. %s", err)
+		}
+
+		var (
+			id         int64
+			macAddress string
+			alias      string
+		)
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"ID", "MAC Address", "Alias"})
+
+		listDB, err := db.Query("SELECT * FROM computers")
+		if err != nil {
+			log.Fatalf("Unable to query database: %s", err)
+		}
+
+		defer listDB.Close()
+		for listDB.Next() {
+			err := listDB.Scan(&id, &macAddress, &alias)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t.AppendRow([]interface{}{id, macAddress, alias})
+			t.AppendSeparator()
+		}
+
+		t.Render()
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
